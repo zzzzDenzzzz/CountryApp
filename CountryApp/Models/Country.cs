b291d434683ca31cf6efde8afcac1cffc4d9ed0b
zzzzDenzzzz <12den24@gmail.com>
2023-10-31 07:55:51 +0300

    fix atribute

--- CountryApp/Models/Country.cs
+++ CountryApp/Models/Country.cs
@@ -7,21 +7,21 @@ namespace CountryApp.Models
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Where Name?"), MaxLength(50)]
+        [Required(ErrorMessage = "Name field is empty"), MaxLength(50)]
         public string Name { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Area field is empty")]
         public decimal Area { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Population field is empty")]
         public decimal Population { get; set; }
 
         public string Description { get; set; }
 
-        [Required, Display(Name = "Image url")]
+        [Required(ErrorMessage = "Image url field is empty"), Display(Name = "Image url")]
         public string ImageUrl { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Parts of the World field is empty"), Display(Name = "Parts of the World")]
         public PartsOfTheWorld PartsOfTheWorld { get; set; }
     }
 }
